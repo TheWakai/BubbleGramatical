20f0a2a999847a2383782b22ccd08a4603ba0a9e
TheWakai <74553156+TheWakai@users.noreply.github.com>
2020-11-19 17:58:38 +0100

    a
    a

--- shoot-bubble-master/Assets/Scripts/Hitter.cs
+++ shoot-bubble-master/Assets/Scripts/Hitter.cs
@@ -5,7 +5,6 @@ public class Hitter : MonoBehaviour
 {
 	public int kind;
 	public GameObject parent;
-	public Sprite specialBubble;
 
 	void Start()
 	{
@@ -14,15 +13,8 @@ public class Hitter : MonoBehaviour
 		{
 			Color[] colorArray = new Color[] { Color.red, Color.cyan, Color.yellow, Color.green, Color.magenta };
 
-			kind = (int)Random.Range(1f, 7f);
-			if (kind == 6)
-			{
-				spriteRenderer.sprite = specialBubble;
-			}
-			else
-			{
-				spriteRenderer.color = colorArray[kind - 1];
-			}
+			kind = (int)Random.Range(1f, 6f);
+			spriteRenderer.color = colorArray[kind - 1];
 		}
 	}
 
